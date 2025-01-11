32c0e02b77475c07b77f5649d01ca6d4bb83f259
Shaheen Acheche <neoshaheen@gmail.com>
2025-01-11 10:50:45 +0100

    ADD bayesian model and notebook

--- footix/models/bayesian.py
+++ footix/models/bayesian.py
@@ -9,21 +9,20 @@ from sklearn import preprocessing
 
 from footix.models.score_matrix import GoalMatrix
 from footix.utils.decorators import verify_required_column
-from footix.utils.utils import DICO_COMPATIBILITY
 
 
 class Bayesian:
-    def __init__(self, n_teams: int):
+    def __init__(self, n_teams: int, n_goals: int):
         self.n_teams = n_teams
+        self.n_goals = n_goals
+        self.label = preprocessing.LabelEncoder()
 
     @verify_required_column(column_names={"HomeTeam", "AwayTeam", "FTR", "FTHG", "FTAG"})
-    def fit(self, x_train: pd.DataFrame):
-        x_train_cop = copy(x_train)
-        if not hasattr(self, "label"):
-            self.label = preprocessing.LabelEncoder()
-            self.label.fit(x_train["HomeTeam"])  # type: ignore
-        x_train_cop["HomeTeamId"] = self.label.transform(x_train["HomeTeam"])
-        x_train_cop["AwayTeamId"] = self.label.transform(x_train["AwayTeam"])
+    def fit(self, X_train: pd.DataFrame):
+        x_train_cop = copy(X_train)
+        self.label.fit(X_train["HomeTeam"])  # type: ignore
+        x_train_cop["HomeTeamId"] = self.label.transform(X_train["HomeTeam"])
+        x_train_cop["AwayTeamId"] = self.label.transform(X_train["AwayTeam"])
 
         goals_home_obs = x_train_cop["FTHG"].to_numpy()
         goals_away_obs = x_train_cop["FTAG"].to_numpy()
@@ -33,17 +32,8 @@ class Bayesian:
 
     def predict(
         self,
-        input_home_team: str,
-        input_away_team: str,
-        score_matrix: bool = False,
-        cote_fdj: bool = True,
-    ) -> tuple[float, np.ndarray] | tuple:
-        if cote_fdj:
-            home_team = DICO_COMPATIBILITY[input_home_team]
-            away_team = DICO_COMPATIBILITY[input_away_team]
-        else:
-            home_team = input_home_team
-            away_team = input_away_team
+        home_team: str,
+        away_team: str) -> GoalMatrix:
 
         team_id = self.label.transform([home_team, away_team])
 
@@ -51,28 +41,27 @@ class Bayesian:
             home_team_id=team_id[0], away_team_id=team_id[1]
         )
 
-        home_probs = stats.poisson.pmf(range(6), home_goal_expectation)
-        away_probs = stats.poisson.pmf(range(6), away_goal_expectation)
+        home_probs = stats.poisson.pmf(range(self.n_goals), home_goal_expectation)
+        away_probs = stats.poisson.pmf(range(self.n_goals), away_goal_expectation)
 
         goals_matrix = GoalMatrix(home_probs, away_probs)
-        home, draw, away = goals_matrix.return_probas()
-
-        if score_matrix:
-            return (home, draw, away), goals_matrix
-        return home, draw, away
+        return goals_matrix
 
     def goal_expectation(self, home_team_id: int, away_team_id: int):
         # get parameters
-        home = np.mean([x[home_team_id] for x in self.trace["home"]])
+
+        home = np.mean(self.trace["home"])
         intercept = np.mean(self.trace["intercept"])
         atts_home = np.mean([x[home_team_id] for x in self.trace["atts"]])
         atts_away = np.mean([x[away_team_id] for x in self.trace["atts"]])
         defs_home = np.mean([x[home_team_id] for x in self.trace["defs"]])
         defs_away = np.mean([x[away_team_id] for x in self.trace["defs"]])
 
+
+
         # calculate theta
-        home_theta = np.exp(intercept + home + atts_home + defs_away)
-        away_theta = np.exp(intercept + atts_away + defs_home)
+        home_theta = np.exp(intercept + home + atts_home - defs_away)
+        away_theta = np.exp(intercept + atts_away - defs_home)
 
         # return the average per team
         return home_theta, away_theta
@@ -85,26 +74,31 @@ class Bayesian:
         away_team: np.ndarray,
     ):
         with pm.Model():
-            home = pm.Normal("home", mu=0, sigma=1, shape=self.n_teams)
+            # Home advantage
+
+            grp_att = pm.Categorical("grp_att", p=[0.5, 0.5], shape=self.n_teams)
+            grp_def = pm.Categorical("grp_def", p=[0.5, 0.5], shape=self.n_teams)
+            # Home advantage and intercept
+            home = pm.Normal("home", mu=0, sigma=1)
             intercept = pm.Normal("intercept", mu=3, sigma=1)
-            # attack ratings
-            tau_att = pm.HalfNormal("tau_att", sigma=2)
-            atts_star = pm.Normal("atts_star", mu=0, tau=tau_att, shape=self.n_teams)
+            # Group-level priors
+            sigma_att = pm.Gamma("sigma_att", alpha=0.1, beta=0.1, shape=2)
+            sigma_def = pm.Gamma("sigma_def", alpha=0.1, beta=0.1, shape=2)
 
-            # defence ratings
-            tau_def = pm.HalfNormal("tau_def", sigma=2)
-            def_star = pm.Normal("def_star", mu=0, tau=tau_def, shape=self.n_teams)
+            # Team-specific attack and defense effects
+            attack = pm.Normal("attack", mu=0, sigma=sigma_att[grp_att], shape=self.n_teams)
+            defense = pm.Normal("defense", mu=0, sigma=sigma_def[grp_def], shape=self.n_teams)
 
-            # apply sum zero constraints
-            atts = pm.Deterministic("atts", atts_star - pt.mean(atts_star))
-            defs = pm.Deterministic("defs", def_star - pt.mean(def_star))
+            # Sum-zero constraints
+            atts = pm.Deterministic("atts", attack - pt.mean(attack))
+            defs = pm.Deterministic("defs", defense - pt.mean(defense))
 
-            # calulate theta
-            home_theta = pt.exp(intercept + home[home_team] + atts[home_team] + defs[away_team])
-            away_theta = pt.exp(intercept + atts[away_team] + defs[home_team])
+            # Calculate theta
+            home_theta = pt.exp(intercept + home + atts[home_team] - defs[away_team])
+            away_theta = pt.exp(intercept + atts[away_team] - defs[home_team])
 
-            # goal expectation
+            # Goal expectation
             pm.Poisson("home_goals", mu=home_theta, observed=goals_home_obs)
             pm.Poisson("away_goals", mu=away_theta, observed=goals_away_obs)
 
-            return pm.sample(2000, tune=500, cores=6, return_inferencedata=False)
+            return pm.sample(3000, tune=2000, chains=4, cores=6, return_inferencedata=False)
