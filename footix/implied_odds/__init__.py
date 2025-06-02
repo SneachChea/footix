2dd33f9e5b86de2d7c7445c975baa3419b32a03c
Shaheen Acheche <neoshaheen@gmail.com>
2025-06-02 22:24:22 +0200

    REFactor implied odds methods: rename functions for consistency and clarity

--- footix/implied_odds/__init__.py
+++ footix/implied_odds/__init__.py
@@ -1,3 +1,3 @@
-from .implied import multiplicative, power, shin
+from .implied import multiplicative_method, power_method, shin_method
 
-__all__ = ["multiplicative", "power", "shin"]
+__all__ = ["multiplicative_method", "power_method", "shin_method"]
